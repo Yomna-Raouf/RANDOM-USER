52073062a1ada0af1edfb630c89dc96b7998c447
yomna raouf <yomnaraouf99@gmail.com>
2020-06-16 21:26:51 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  useEffect( async () => {
+    // this code runs once when the component loads 
+    // Here I'm going to fetch a random user from the random user generator API
+    const fetchData = async () => {
+      let response = await fetch('https://randomuser.me/api/api');
+      if(response.ok) {
+        let json = await response.json();
+      } else {
+        alert('HTTP-Eroor' + response.status);
+      }
+
+    } 
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Random user generator</h1>
     </div>
+    
   );
 }
 
